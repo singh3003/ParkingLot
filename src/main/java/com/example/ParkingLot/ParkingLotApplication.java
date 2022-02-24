3cb8c78a0f83df8740608af6774d2aa8cdef3ece
Shivani <shivanis3003@gmail.com>
2022-02-25 00:33:30 +0530

    adding file

---
+++ src/main/java/com/example/ParkingLot/ParkingLotApplication.java
@@ -0,0 +1,14 @@
+package com.example.ParkingLot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApplication {
+
+	public static void main(String[] args) {
+		System.out.println("start of the application");
+		SpringApplication.run(ParkingLotApplication.class, args);
+	}
+
+}
